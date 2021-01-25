82ef713dd433c30c828119419ff00de15cd9203b
Dookyeong Kang <lapis@postech.ac.kr>
2021-01-25 21:27:43 +0900

    saved

--- p1238.py
+++ p1238.py
@@ -11,7 +11,7 @@ for i in range(N):
 
 for j in range(N):
     for i in range(N):
-        if dist[i][j] != MAX:
+        if dist[j][i] != MAX:
             for k in range(N):
                 dist[k][i] = min(dist[k][i], dist[k][j] + dist[j][i])
 ret = 0
